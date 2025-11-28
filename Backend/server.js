6bf22d7007fdf58dadfb9b29c58b4c2f50c10f59
Asasing37 <samuelcuidaesquivel@gmail.com>
2025-11-27 22:35:27 -0500

    Llamado a la base de datos local creado

--- Backend/server.js
+++ Backend/server.js
@@ -8,3 +8,27 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//Carpeta de imagenes.
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+//Manejo de rutas inexistentes.
+
+app.use((req, res, next) => {
+    res.status(404).json({error: 'Ruta no encontrada'});
+});
+
+//Manejo de errores internos del servidor.
+
+app.use((err,req, res, next) => {
+    console.error('Error en el servidor: ', err);
+    res.status(500).json({error: 'Error interno del servidor'});
+});
+
+//Llamado al puerto.
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+});
